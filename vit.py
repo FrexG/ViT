c6f26d813cc3aeff1e907672d5fc0e04e014d6e6
Fraol Gelana <fraolgela@gmail.com>
2023-03-17 14:38:04 +0300

    classifier head

--- vit.py
+++ vit.py
@@ -11,7 +11,7 @@ class PatchEmbedding(nn.Module):
 
         self.cls_token = nn.Parameter(torch.rand(1,1,emb_size))
 
-        self.position_encoding = nn.Parameter(torch.randn(img_size // patch_size) ** 2 + 1,emb_size))
+        self.position_encoding = nn.Parameter(torch.randn((img_size // patch_size) ** 2 + 1,emb_size))
 
     def forward(self,x:torch.Tensor):
 
@@ -32,8 +32,9 @@ class PatchEmbedding(nn.Module):
         return x + self.position_encoding
 
 class MultiHeadAttention(nn.Module):
-    def __init__(self,emb_size:int = 512,num_heads:int = 8,dropout:float = 0.0) -> None:
+    def __init__(self,emb_size:int = 512,num_heads:int = 8,drop_out:float = 0.0) -> None:
         super().__init__()
+        print(type(drop_out))
         self.emb_size = emb_size
 
         self.num_heads = num_heads
@@ -46,7 +47,7 @@ class MultiHeadAttention(nn.Module):
 
         self.values= nn.Linear(emb_size,emb_size)
 
-        self.att_drop = nn.Dropout(dropout)
+        self.att_drop = nn.Dropout(drop_out)
 
         self.projection = nn.Linear(emb_size,emb_size)
 
@@ -79,15 +80,10 @@ class MultiHeadAttention(nn.Module):
 
         out = out.view(b,seq_len,self.num_heads * self.head_dim)
 
-class ResidualAdd(nn.Module):
-    def __init__(self,emb_size:int = 512) -> None:
-        super().__init__()
-        self.mha = MultiHeadAttention(emb_size = emb_size)
-    def forward(self,x):
-        return x + self.mha(x)
+        return out
     
 class MLP(nn.Module):
-    def __init__(self,emb_size:int = 512,expansion:int = 4,drop_out:float = 0.0):
+    def __init__(self,emb_size:int = 768,expansion:int = 4,drop_out:float = 0.0):
         super().__init__()
         self.feed_forward = nn.Sequential(
             nn.Linear(emb_size,expansion * emb_size),
@@ -97,11 +93,69 @@ class MLP(nn.Module):
         )
     def forward(self,x):
         return self.feed_forward(x)
+    
+class TransformerEncoderBlock(nn.Module):
+    def __init__(self,emb_size:int = 768,drop_out:float = 0.0
+                 ,forward_expansion:int = 4,forward_drop_out:float = 0.0,):
+        super().__init__()
+        
+        self.attention = nn.Sequential(
+                nn.LayerNorm(emb_size),
+                MultiHeadAttention(emb_size,drop_out=drop_out),
+                nn.Dropout(drop_out)
+            )
+        self.feed_forward = nn.Sequential(
+                nn.LayerNorm(emb_size),
+                MLP(emb_size=emb_size,expansion=forward_expansion,drop_out=forward_drop_out),
+                nn.Dropout(drop_out)
+            )
+    def forward(self, x):
+        x_att = x + self.attention(x)
+        out = x_att + self.feed_forward(x_att)
+        return out
+    
+class TransformerEncoder(nn.Module):
+    def __init__(self,depth=4):
+        super().__init__()
+        self.transformer = nn.ModuleList([TransformerEncoderBlock() for i in range(depth)])
+
+    def forward(self,x):
+        for t in self.transformer:
+            x = t(x)
+        return x
+
+class ClassificationHead(nn.Module):
+    def __init__(self,emb_size:int = 768,n_classes:int = 3):
+        super().__init__()
+
+        self.clf = nn.Sequential(
+            nn.LayerNorm(emb_size),
+            nn.Linear(emb_size,n_classes)
+        )
+        
+    def forward(self,x):
+        x = self.clf(torch.mean(x,dim=1))
+        return x
+
+
+class ViT(nn.Module):
+    def __init__(self,emb_size:int = 768,n_classes:int = 3,img_size:int = 224,depth:int = 4) -> None:
+        super().__init__()
+        self.patch_embedd = PatchEmbedding(img_size=img_size)
+        self.transformer_encoder = TransformerEncoder(depth)
+        self.clf_head = ClassificationHead(emb_size,n_classes=n_classes)
+
+    def forward(self,x):
+        x = self.transformer_encoder(self.patch_embedd(x))
+        x = self.clf_head(x)
+        return x
+        
 
 
 if __name__ == "__main__":
     x = torch.randn(4,3,224,224)
 
-    patch_embedd = PatchEmbedding()
+    vit = ViT(img_size = 224)
 
-    y = patch_embedd(x)
+    y = vit(x)
+    print(y.shape)
